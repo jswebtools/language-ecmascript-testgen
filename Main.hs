87912f7ab32e4a36496c9d82a5e2ebd45124e9bd
Andrey Chudnov <oss@chudnov.com>
2015-12-04 17:05:03 -0500

    Specify the file-name

--- Main.hs
+++ Main.hs
@@ -6,13 +6,18 @@ import Data.Aeson
 import qualified Data.ByteString.Lazy as BS
 import System.IO
 import System.Exit
+import System.Environment
 import Converter
 
 main :: IO ()
-main = do json <- BS.getContents
+main = do [fileName] <- getArgs
+          json <- BS.readFile fileName
           case eitherDecode json of
            Left msg -> hPutStrLn stderr ("AST parse failure: " ++ msg)
                        >> exitFailure
-           Right p  -> case convert p of
-                        Right es -> putStrLn (show es) >> exitSuccess
-                        Left err -> hPutStrLn stderr err >> exitFailure
+           Right p  ->
+             do putStrLn $ "Read JSON successfully: " ++ (show p)
+                putStrLn "Converting..."
+                case convert p of
+                 Right es -> putStrLn (show es) >> exitSuccess
+                 Left err -> hPutStrLn stderr err >> exitFailure
