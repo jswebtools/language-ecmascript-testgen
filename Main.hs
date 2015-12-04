2f36142f3585db7bd1da99c44fa261c3bcbca304
Andrey Chudnov <oss@chudnov.com>
2015-12-04 18:18:42 -0500

    remove the debug printing

--- Main.hs
+++ Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 -- | Reads a Mozilla SpiderMonkey Parser API AST from the stdin and
 -- produces a source representation of language-ecmascript parser AST.
 module Main where
@@ -7,17 +8,16 @@ import qualified Data.ByteString.Lazy as BS
 import System.IO
 import System.Exit
 import System.Environment
+import Control.Monad
 import Converter
 
 main :: IO ()
 main = do [fileName] <- getArgs
           json <- BS.readFile fileName
+          when ("FAIL" `BS.isPrefixOf` json) $ putStrLn "Nothing" >> exitSuccess
           case eitherDecode json of
            Left msg -> hPutStrLn stderr ("AST parse failure: " ++ msg)
                        >> exitFailure
-           Right p  ->
-             do putStrLn $ "Read JSON successfully: " ++ (show p)
-                putStrLn "Converting..."
-                case convert p of
-                 Right es -> putStrLn (show es) >> exitSuccess
-                 Left err -> hPutStrLn stderr err >> exitFailure
+           Right p  -> case convert p of
+                        Right es -> putStrLn (show (Just es)) >> exitSuccess
+                        Left err -> hPutStrLn stderr err >> exitFailure
